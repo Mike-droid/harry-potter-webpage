b1127ebc124195b81f7cdff0b0420e32a53df857
Miguel Angel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2021-08-24 21:57:01 -0500

    Page working correctly

---
+++ app.ts
@@ -0,0 +1,39 @@
+const apiURL: string = 'https://hp-api.herokuapp.com/api/characters';
+const charactersDiv = <HTMLDivElement>document.getElementById('characters');
+const getDataButton = <HTMLButtonElement>document.getElementById('getData_button');
+const houses = <HTMLSelectElement>document.getElementById('houses');
+
+async function getData () {
+  const response = await fetch(apiURL);
+  const data = await response.json();
+
+  function fillData() {
+    for (let index = 0; index < data.length; index++) {
+      const characterCard = `
+        <div class="character ${houses.value}">
+          <h2>${data[index].name}</h2>
+          <img src="${data[index].image}" />
+          <p>Wand core: ${data[index].wand.core}</p>
+          <p>Wand length: ${data[index].wand.length} inches</p>
+          <p>Wand wood: ${data[index].wand.wood}</p>
+          <p>Patronus: ${data[index].patronus}</p>
+        </div>
+      `
+      if (data[index].house === houses.value) {
+        charactersDiv.innerHTML += characterCard;
+      } else if (houses.value === 'none') {
+        charactersDiv.innerHTML = `<h2>Please select a Hogwart's House.</h2>`;
+      }
+    }
+  }
+
+  if (charactersDiv.innerHTML === '') {
+    fillData();
+  } else {
+    charactersDiv.innerHTML = '';
+    fillData();
+  }
+
+}
+
+getDataButton?.addEventListener('click', getData);
